23120408a68cc3915ccd457b5f369612ac7a73d1
Krish <andresources1039@gmail.com>
2024-03-04 10:21:45 +0530

    Toggle

--- app/src/main/java/com/kotlincourse/telephonymanager/TelephonyManagerActivity.kt
+++ app/src/main/java/com/kotlincourse/telephonymanager/TelephonyManagerActivity.kt
@@ -2,11 +2,21 @@ package com.kotlincourse.telephonymanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.kotlincourse.R
 
 class TelephonyManagerActivity : AppCompatActivity() {
+    lateinit var btnSMS: Button
+    lateinit var btnMail: Button
+    lateinit var btnCall: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_telephony_manager)
+        btnSMS = findViewById(R.id.btnSMS)
+        btnMail = findViewById(R.id.btnMail)
+        btnCall = findViewById(R.id.btnCall)
+        btnSMS.setOnClickListener {
+
+        }
     }
 }
