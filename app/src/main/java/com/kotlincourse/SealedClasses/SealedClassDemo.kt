251582410728a12edf9c344b4bd3d13511b4b687
Krish <andresources1039@gmail.com>
2024-03-16 20:48:52 +0530

    Enum

--- app/src/main/java/com/kotlincourse/SealedClasses/SealedClassDemo.kt
+++ app/src/main/java/com/kotlincourse/SealedClasses/SealedClassDemo.kt
@@ -1,14 +1,14 @@
 package com.kotlincourse.SealedClasses
 
-import com.kotlincourse.enums.Play
-
 sealed class Shape{
     class Circle(var radius: Float): Shape()
     class Square(var length: Int): Shape()
     class Rectangle(var length: Int, var breadth: Int): Shape()
 }
+
+
 fun main(){
-    var e: Shape = Shape.Circle(10.5f)
+    var e: Shape = Shape.Rectangle(10,5)
     when (e) {
         is Shape.Circle -> println("Circle area is ${3.14*e.radius*e.radius}")
         is Shape.Square -> println("Square area is ${e.length*e.length}")
--- app/src/main/java/com/kotlincourse/enums/enumdemo.kt
+++ app/src/main/java/com/kotlincourse/enums/enumdemo.kt
@@ -2,22 +2,26 @@ package com.kotlincourse.enums
 
 fun main()
 {
-    /*var a = "test"
+    /*var a = "test" //a,b,c
     a = "t2"
-    a = "t3"*/
-    var play = Play.START
-    var message = when(play)
-    {
-        Play.START -> "Music started"
-        Play.PAUSE -> "Music paused"
-        Play.STOP -> "Music stopped"
-    }
-    println(message)
+    a = "t3"
+    a = "z"
+    a = "xyz"*/
+
+    var a:MediaPlayer1  = MediaPlayer1.STOP
+    when(a){
+        MediaPlayer1.START -> { println("Music Started")}
+        MediaPlayer1.PAUSE -> { println("Music paused")}
+        MediaPlayer1.RESUME -> { println("Music resumed")}
+        MediaPlayer1.STOP -> { println("Music stopped")}
+        }
 }
 
-enum class Play{
-    START, PAUSE, STOP
+enum class MediaPlayer1{
+    //predefined constants
+    START, PAUSE, RESUME, STOP
 }
+
 
 enum class Weekend(val value: Int) {
     SATURDAY(0), SUNDAY(1),MONDAY(2)
